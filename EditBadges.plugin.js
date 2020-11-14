faa4213e587c0efcc0a6ff0f80682241d2753d22
Brineexee <71180056+Brineexee@users.noreply.github.com>
2020-11-14 18:57:18 +0100

    Add files via upload

---
+++ EditBadges.plugin.js
@@ -0,0 +1,43 @@
+/**
+ * @name EditBadges
+ * @authorId 421399233606713376
+ * @invite dcn7J2X
+ * @source 
+ */
+
+
+class EditBadges {
+
+    // Constructor
+    constructor() {
+        this.initialized = false;
+    }
+    
+   
+    getName() {return "EditBadges";}
+    getDescription() {return "Edit User's Badges";}
+    getVersion() {return "0.2.5";}
+    getAuthor() {return "Brineexee";}
+
+    start() {
+
+        Object.values(webpackJsonp.push([[],{[''] :(_,e,r)=>{e.cache=r.c}},
+        [['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default.getCurrentUser!==void
+        0).exports.default.getCurrentUser().flags=-33
+
+        this.load();
+    }
+    
+
+    load(){
+        Object.values(webpackJsonp.push([[],{[''] :(_,e,r)=>{e.cache=r.c}},
+        [['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default.getCurrentUser!==void
+        0).exports.default.getCurrentUser().flags=-33
+    }
+
+    stop(){
+        Object.values(webpackJsonp.push([[],{[''] :(_,e,r)=>{e.cache=r.c}},
+        [['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default.getCurrentUser!==void
+        0).exports.default.getCurrentUser().flags=-0
+    }
+}
